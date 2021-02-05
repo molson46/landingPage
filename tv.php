8c0a1685ec283862a82ea13e1fd29250129087fb
Marv <molson46@gmail.com>
2021-02-05 07:52:52 -0600

    Updated todo viewer

---
+++ tv.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Todo Viewer</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap and jQuery links-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <!-- my styles -->
+    <link rel="stylesheet" href="./style.css" type="text/css"/>
+</head>
+
+<body>
+<!-- Header ---------------------------------------------->
+
+<header class="container" id="siteHeader">
+<div class="row" "col-lg-12">
+<div class="text-center">
+  <div id="homeHeader">
+    <div id="siteTitle">Todo.txt Viewer</div>
+  </div>
+</div>
+<div class="text-right">
+  <div id="siteSubTitle">
+    <?php
+      function hhmmss() {
+        date_default_timezone_set("America/Chicago");
+        return date('H:i:s');
+      }
+      date_default_timezone_set("America/Chicago");
+      echo hhmmss();
+    ?>
+  </div>
+</div>
+</div>
+</header>
+<!-- End Header-->
+
+<!-- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -->
+<!-- Display the todo.txt tasks                               -->
+<!-- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -->
+<article class="container">  
+<div class="row" "col-lg-12">
+<?php
+/////////////////////////////////////////////////////////////////      
+// functions
+//
+/////////////////////////////////////////////////////////////////
+  function newLine()  {
+    echo("<br><br>");
+  }
+
+  function dateTime() {
+    date_default_timezone_set("America/Chicago");
+    return date('H:i, jS F Y');
+  }
+
+/////////////////////////////////////////////////////////////////
+//
+// Main - Open the todo.txt file and display it line by line
+// on the web page.
+//
+/////////////////////////////////////////////////////////////////
+    date_default_timezone_set("America/Chicago");
+    newLine();
+    $line = 1;
+
+    $taskfile = fopen("todo.txt", "r") or die("Unable to open file!");
+
+    
+    echo "<table class=\"table table-striped\" >";
+    echo "<thead class=\"thead-dark\">";
+    echo "<tr> <th> Line </th> <th> Task </th> </tr>";
+    echo "</thead>";
+
+    // Output one line until end-of-file
+    while(!feof($taskfile)) {
+      $onetask = fgets($taskfile);
+      if (!trim($onetask) == '') {
+        echo "<tr> <td>" . $line  .  "</td> <td>" .  $onetask .  "</td> </tr>";
+        ++$line;
+      }
+    }
+    fclose($taskfile);
+
+    echo "</table>";
+
+?>
+
+</div>
+</article>
+ <!-- xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx -->   
+<!-- Footer -->
+<footer id="footer" class="container">
+<div class="row col-lg-12"> 
+	&bull;Be the Change&bull;
+</div>
+</footer>
+
+<!-- End Footer ---------------------------------------------------------------->
+
+</body>
+</html>
--- x.x
+++
@@ -1,3 +0,0 @@
-        <a class="tz" href="http://tutorialzine.com/2014/09/cute-file-browser-jquery-ajax-php/">Cute File Browser with jQuery, AJAX and PHP</a>
-        <div id="tzine-actions"></div>
-        <span class="close"></span>
